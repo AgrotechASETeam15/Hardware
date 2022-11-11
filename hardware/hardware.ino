05b5d9a32e14f11ef1835ffc92098b8d4bce8721
mohit41999 <mohit41999@gmail.com>
2022-11-11 02:27:21 -0500

    SMOKE DETECTOR

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -1,9 +1,15 @@
 float sensorlevel1;
+const int buzzer = 10;
+const int redLed = 13;
+int sensorThres = 80;
 
 void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
   Serial.begin(9600);
 }
-
 void dripIrrigation() {
   int sensorvalue1 = analogRead(A3);
   Serial.println(sensorvalue1);
@@ -17,7 +23,30 @@ void dripIrrigation() {
       Serial.print("Solenoid Valve 1 Close \n");
   }   
 }
+void smokeDetector(){
+  int analogSensor = analogRead(A2);
 
-void loop() {
-  dripIrrigation();
+  Serial.print("Smoke : ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+    delay(100);  
+    digitalWrite(redLed, LOW);
+    delay(100);
+  
+    tone(buzzer, 1000, 200);
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    noTone(buzzer);
+  }
 }
+void loop() {
+  
+  dripIrrigation();
+  smokeDetector();
+}
+
