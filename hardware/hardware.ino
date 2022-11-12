3ded144d11a5f50e302711c4c088305961bcfc0e
Sachin Desai <sachin41999@gmail.com>
2022-11-11 22:38:12 -0500

    First Commit Sachin

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -3,27 +3,33 @@ const int buzzer = 10;
 const int redLed = 13;
 int sensorThres = 80;
 
-void setup() {
+void setup()
+{
   pinMode(redLed, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
   Serial.begin(9600);
 }
-void dripIrrigation() {
+void dripIrrigation()
+{
   int sensorvalue1 = analogRead(A3);
   Serial.println(sensorvalue1);
-  sensorlevel1 = (sensorvalue1/577.00)*100;
-  Serial.print("Moisture : "); 
-  Serial.println(sensorlevel1); 
+  sensorlevel1 = (sensorvalue1 / 577.00) * 100;
+  Serial.print("Moisture : ");
+  Serial.println(sensorlevel1);
   Serial.print("\n");
-  if(sensorlevel1 >= 50.00){ 
-      Serial.print("Solenoid Valve 1 Open \n");
-  }else{ 
-      Serial.print("Solenoid Valve 1 Close \n");
-  }   
+  if (sensorlevel1 >= 50.00)
+  {
+    Serial.print("Solenoid Valve 1 Open \n");
+  }
+  else
+  {
+    Serial.print("Solenoid Valve 1 Close \n");
+  }
 }
-void smokeDetector(){
+void smokeDetector()
+{
   int analogSensor = analogRead(A2);
 
   Serial.print("Smoke : ");
@@ -32,10 +38,10 @@ void smokeDetector(){
   if (analogSensor > sensorThres)
   {
     digitalWrite(redLed, HIGH);
-    delay(100);  
+    delay(100);
     digitalWrite(redLed, LOW);
     delay(100);
-  
+
     tone(buzzer, 1000, 200);
   }
   else
@@ -44,9 +50,9 @@ void smokeDetector(){
     noTone(buzzer);
   }
 }
-void loop() {
-  
+void loop()
+{
+
   dripIrrigation();
   smokeDetector();
 }
-
